bc06f4908e4780209b8fff521ba59a136f4ab31d
Jamaal Pierce <piercer103@gmail.com>
2020-05-31 23:22:13 +0000

    My reason for taking CS407

--- Name.java
+++ Name.java
@@ -1,5 +1,5 @@
 public class Name{
 	public static void main(String[] args){
-		System.out.println("My favorite programming language is Python.");
+		System.out.println("I am taking CS407 because I want to see what more I can do when it comes to programming on my raspberry pi, as well as some knowledge and practice with programming.");
 	}
 }
