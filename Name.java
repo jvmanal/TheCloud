4013ab4f2a53d7852b7c7dcf14614a33ba2aa8fa
Jamaal Pierce <piercer103@gmail.com>
2020-05-31 23:24:54 +0000

    My favorite food

--- Name.java
+++ Name.java
@@ -1,5 +1,5 @@
 public class Name{
 	public static void main(String[] args){
-		System.out.println("I am taking CS407 because I want to see what more I can do when it comes to programming on my raspberry pi, as well as some knowledge and practice with programming.");
+		System.out.println("My favorite food is Baked Thai Pineapple Salmon.");
 	}
 }
